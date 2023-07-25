135dc72d7087a0c0ef9966afa8b10f63a492bad0
pdh9311 <padohy@gmail.com>
2023-07-25 10:13:02 +0900

    1

---
+++ src/main/java/study/corespringsecurity/controller/user/UserController.java
@@ -0,0 +1,15 @@
+package study.corespringsecurity.controller.user;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/mypage")
+	public String myPage() throws Exception {
+
+		return "user/mypage";
+	}
+}
---
+++ src/test/java/study/corespringsecurity/CoreSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package study.corespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
